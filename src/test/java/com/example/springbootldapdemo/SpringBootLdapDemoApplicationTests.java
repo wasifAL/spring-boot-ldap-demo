902247c1f7a2279fb7d342350054a8ef2f21674f
Wasif Al Mahmud <wamvmaw@gmail.com>
2022-02-24 03:56:34 +0600

    initiated project

---
+++ src/test/java/com/example/springbootldapdemo/SpringBootLdapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootldapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLdapDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
