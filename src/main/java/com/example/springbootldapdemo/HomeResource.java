fcb5e18a9a5de8e1768f69950f21154f8a9aea43
Wasif Al Mahmud <wamvmaw@gmail.com>
2022-02-24 04:25:17 +0600

    home controller with single access point security configured to use ldap information

---
+++ src/main/java/com/example/springbootldapdemo/HomeResource.java
@@ -0,0 +1,12 @@
+package com.example.springbootldapdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+    @GetMapping("/")
+    public String index(){
+        return "Home Page";
+    }
+}
